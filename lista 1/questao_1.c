57dece31b69a8b7d8455ae0b261f3ef6c10168f4
Hugo <92953811+vhugodv@users.noreply.github.com>
2023-02-28 21:19:53 -0300

    Update questao_1.c

--- lista 1/questao_1.c
+++ lista 1/questao_1.c
@@ -1,4 +1,5 @@
-/*Diga a ordem de cálculo dos operadores em cada uma das instruções em C a seguir e mostre o valor
+/*
+Diga a ordem de cálculo dos operadores em cada uma das instruções em C a seguir e mostre o valor
 de x depois que cada instrução for executada.
 
 Item 1: x = 7+3*6/2-1;
